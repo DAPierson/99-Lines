2fd838f2bd4e067b3251cfc7a399a8626ffe5374
Dustin Pierson <dustinapierson@gmail.com>
2018-01-10 15:26:25 -0600

    finished project

--- apps.js
+++ apps.js
@@ -1,8 +1,20 @@
 let friends = ['dustin', 'will', 'josh', 'alysia', 'todd'];
-for(let i = 0; i < friends.length; i++){
-    for (let j = 99; j > 1; j--) {
-        console.log( j + ' lines of code in the file, ' + j + ' lines of code; ' + friends[i] + " strikes one out, clears it all out, ");
-    
-    }
-}
 
+
+
+for (let i = 0; i < friends.length; i++) {
+    console.log(friends[i]+":");
+    
+    for (let j = 99; j > 0; j--) {
+         if (j > 1) {
+            let song = j + ' lines of code in the file, ' + j + ' lines of code; ' + friends[i] + " strikes one out, clears it all out, "
+            console.log(song)
+         }
+        else  {
+           let song1 = j + ' line of code in the file, ' + j + ' line of code; ' + friends[i] + " strikes one out, clears it all out, no more lines of code in the file "
+         console.log(song1)
+         }
+
+    }
+
+}
