eb67c2435147be6b333ca527a2c98e79d8c348bf
Dustin Pierson <dustinapierson@gmail.com>
2018-01-10 14:55:43 -0600

    need to fix line1

--- apps.js
+++ apps.js
@@ -1,6 +1,6 @@
 let friends = ['dustin', 'will', 'josh', 'alysia', 'todd'];
 for(let i = 0; i < friends.length; i++){
-    if ( j = 99; j > 1; j--) {
+    for (let j = 99; j > 1; j--) {
         console.log( j + ' lines of code in the file, ' + j + ' lines of code; ' + friends[i] + " strikes one out, clears it all out, ");
     
     }
